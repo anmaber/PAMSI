b1edcaf78af9b26af10e4fce05057a4c8aba8cef
Anna Bernas <bernas.anna@icloud.com>
2019-03-07 18:40:58 +0100

    added push_back to the list but causes Segmentation falut

--- project1/list.hpp
+++ project1/list.hpp
@@ -1,10 +1,56 @@
 #pragma once
 
 #include "node.hpp"
+#include <iostream>
 
 template<class Type>
 class List
 {
+
+
+public:
+
     Node<Type>* head;
     Node<Type>* tail;
+
+    List();
+    ~List();
+    void push_back(const Type& newElement);
+
 };
+
+template<class Type>
+List<Type>::List()
+     :head(nullptr),
+      tail(nullptr)
+{}
+
+template<class Type>
+List<Type>::~List()
+{
+    while(head)
+    {
+        Node<Type>* next = head->next;
+        delete head;
+        head = next;
+    }
+}
+
+template<class Type>
+void List<Type>::push_back(const Type &newElement)
+{
+    Node<Type>* node = new Node<Type>(newElement);
+
+    if(!head)
+    {
+        head=node;
+        tail=node;
+    }
+    else
+    {
+        node->previous = tail;
+        tail->next = node;
+        tail = node;
+    }
+
+}
--- project1/main.cpp
+++ project1/main.cpp
@@ -3,5 +3,7 @@
 int main()
 {
     List<int> list;
+    list.push_back(4);
+    list.push_back(1);
     return 0;
 }

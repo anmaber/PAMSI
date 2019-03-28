00f076a91ed48ea96b1982b06c3eeaae16397149
Anna Bernas <bernas.anna@icloud.com>
2019-03-28 12:35:30 +0100

    implemented merge function template

--- project2/inc/merge.hpp
+++ project2/inc/merge.hpp
@@ -1,1 +1,47 @@
 #pragma once
+
+template <typename arrayType>
+int merge(arrayType A[], int begin, int mid, int end)
+{
+    int leftSize = mid - begin + 1;
+    int rightSize = end - mid;
+
+    arrayType left[leftSize];
+    arrayType right[rightSize];
+
+    for(int i = 0; i < leftSize; ++i) left[i] = A[begin+i];
+    for(int i = 0; i < rightSize; ++i) right[i] = A[mid + 1 + i];
+
+    int i = 0;
+    int j = 0;
+    int k = begin;
+
+    while (i < leftSize && j < rightSize)
+    {
+        if (left[i] <= right[j])
+        {
+            A[k] = left[i];
+            i++;
+        }
+        else
+        {
+            A[k] = right[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < leftSize)
+    {
+        A[k] = left[i];
+        i++;
+        k++;
+    }
+
+    while (j < rightSize)
+    {
+        A[k] = right[j];
+        j++;
+        k++;
+    }
+}

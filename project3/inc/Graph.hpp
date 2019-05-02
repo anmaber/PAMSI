8f1e06d6c53f71dc2e0b160034e283d73b1e9417
Anna Bernas <bernas.anna@icloud.com>
2019-05-02 14:55:20 +0200

    implemented constructor and addNeighbour method

--- project3/inc/Graph.hpp
+++ project3/inc/Graph.hpp
@@ -6,11 +6,14 @@ using AdjacencyList = std::vector<std::vector<std::pair<int,int>>>;
 
 class Graph{
 
-    int vertexNumber;
-    AdjacencyList adjacency;
+    int vertexNumber_;
+    int begin_;
+    AdjacencyList adjacency_;
 
 public:
 
+    Graph(int vertexNumber, int begin);
     void addNeighbour(int nodeNumber, int neighbourNumber, int pathCost);
+    void dijkstra();
 
 };
--- project3/src/Graph.cpp
+++ project3/src/Graph.cpp
@@ -1,1 +1,13 @@
 #include "Graph.hpp"
+
+Graph::Graph(int vertexNumber, int begin)
+    : vertexNumber_(vertexNumber),
+      begin_(begin)
+
+{}
+
+void Graph::addNeighbour(int nodeNumber, int neighbourNumber, int pathCost)
+{
+    adjacency[nodeNumber].emplace_back(std::make_pair(neighbourNumber,pathCost));
+}
+
